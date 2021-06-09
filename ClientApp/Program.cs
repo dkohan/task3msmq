7f5a17ca25eb62933f6a73c2a0db27794fb9f8b5
Dzmitriy Kakhanouski <dzmitriy_kakhanouski@epam.com>
2021-06-09 16:10:10 +0300

    first commit

---
+++ ClientApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Messaging;
+
+namespace ClientApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(@"C:\\FilesForMessage");
+
+            watcher.EnableRaisingEvents = true;
+            watcher.Created += Watcher_Created;
+
+            Console.ReadKey();
+        }
+
+        private static void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            MessageQueue msgQ = new MessageQueue(".\\Private$\\epamtestqueue");
+
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
+
+            byte[] bytes = File.ReadAllBytes(e.FullPath);
+
+            Message msg = new Message();
+            msg.Label = e.Name;
+            msg.Body = bytes;  
+            
+            msgQ.Send(msg);
+        }
+    }
+
+    public class CustomMessage
+    {
+        public string Name { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Messaging;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server started.");
+
+            MessageQueue queue = new MessageQueue(".\\Private$\\epamtestqueue");
+            queue.Formatter = new XmlMessageFormatter(new Type[] { typeof(byte[]) });
+
+            queue.ReceiveCompleted += Queue_ReceiveCompleted; ;
+            queue.BeginReceive();
+
+            Console.ReadKey();
+
+        }
+
+        private static void Queue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            MessageQueue mq = (MessageQueue)sender;
+            if (mq != null)
+            {
+                var message = e.Message;
+
+                var fileName = message.Label;
+                var content = (byte[])message.Body;
+
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(content, 0, content.Length);
+                }
+
+                Console.WriteLine("File:" + fileName + "is created!");
+            }
+        }
+    }
+}
